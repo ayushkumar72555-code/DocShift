c82a67aa238e36d1edcadb8a3c8be96900107e8c
ayushk <ayushkumar72555@gmail.com>
2026-09-22 00:06:25 +0530

    Release DocShift 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ayush.docshift"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.1.0"
+        versionCode = 2
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
