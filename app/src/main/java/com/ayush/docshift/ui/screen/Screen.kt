1f1137a32fbd5edaa14fb74bcb4530ba0ef5cae4
ayushk <ayushkumar72555@gmail.com>
2026-09-22 14:10:52 +0530

    Add DocSafe navigation route

--- app/src/main/java/com/ayush/docshift/ui/screen/Screen.kt
+++ app/src/main/java/com/ayush/docshift/ui/screen/Screen.kt
@@ -3,6 +3,7 @@ package com.ayush.docshift.ui.screen
 sealed class Screen {
     object Home : Screen()
     object Tutorial : Screen()
+    object DocSafe : Screen()
     object Compress : Screen()
     object Resize : Screen()
     object PdfCompress : Screen()
