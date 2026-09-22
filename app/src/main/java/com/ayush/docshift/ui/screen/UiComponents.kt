b0d7820d5a13f84e6805665ecf77bdca429cb9e5
ayushk <ayushkumar72555@gmail.com>
2026-09-22 15:49:54 +0530

    Opt in to Material 3 top app bar API

--- app/src/main/java/com/ayush/docshift/ui/screen/UiComponents.kt
+++ app/src/main/java/com/ayush/docshift/ui/screen/UiComponents.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DocShiftScaffold(
     title: String,
